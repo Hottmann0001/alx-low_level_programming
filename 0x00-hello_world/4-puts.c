04e7c635c475d0f5baec613842cb40dd91ea8b09
Hottmann0001 <mustafahotman0001@gmail.com>
2023-09-08 18:35:32 -0700

    dd

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this function is for practicier
+ * Return: Always zero
+ *Description: A C program that Print by using puts
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
