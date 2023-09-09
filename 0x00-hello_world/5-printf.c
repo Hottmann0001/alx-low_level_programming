4083d6d4fd529b5ed173a8ac85bd1daa7c14154d
Hottmann0001 <mustafahotman0001@gmail.com>
2023-09-08 19:01:15 -0700

    ss

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - this function is for practicier
+ * main: this function is for practicier
  * Return: Always zero
  *Description: A C program that Print by using printf
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
