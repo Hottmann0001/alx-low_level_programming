61462e8fb1912ed147e745c82206ff46e481cd2d
Hottmann0001 <mustafahotman0001@gmail.com>
2023-09-18 07:29:48 -0700

    s

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,43 +1,26 @@
-#include "main.h"
-
-/**
- * print_number - print any integer using putchar
- * @n: integer to be printed
+/*
+ * File: 101-print_number.c
+ * Auth: Brennan D Baraban
  */
 
+#include "holberton.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
 void print_number(int n)
 {
-	int count, x, y, k;
+	unsigned int num = n;
 
-	x = 0;
-	y = 1000000000;
-
-	if (n == 0)
-		_putchar('0');
-	else if (n > 0)
-		n *= -1;
-	else
+	if (n < 0)
+	{
 		_putchar('-');
-	for (count = 0; count < 10; count++)
-{
-		if (n / y == 0 && x == 0)
-		{
-			y /= 10;
+		num = -num;
+	}
 
-			continue;
-		}
-		else if (x == 0)
-		{
-			_putchar(-(n / y) + '0');
-			x += 1;
-i		}
-		else
-		{
-			k = (-(n / y) % 10);
-			if (k < 0)
-				k *= -1;
-			_putchar(k + '0');
-		}
-		y /= 10;
-}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
