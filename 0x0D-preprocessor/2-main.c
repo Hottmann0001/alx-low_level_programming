ec82738f98f7e39393f1a3c38e9ff7e12a73840b
mustafa <mustafaelskhon79@gmail.com>
2023-10-12 01:53:48 +0300

    first C

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ *main -Entry point
+ *Return: 0 (Success)
+ *Description: return the current file directory 
+ */
+
+int main()
+{
+printf("%s \n",__FILE__);
+return(0);
+
+
+}
