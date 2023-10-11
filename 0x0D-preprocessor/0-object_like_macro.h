5ea659fc5e29f2166c4dcc01e47815bb7b084ade
mustafa <mustafaelskhon79@gmail.com>
2023-10-12 01:57:34 +0300

    2nd C

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,10 @@
 #ifndef _OBJECT_LIKE_MACRO_H
 #define _OBJECT_LIKE_MACRO_H
+/**
+ * File: O-object_like_macro
+ *
+ * Desc: haeder file
+ */
 #define SIZE 1024
-#endif 
+
+#endif /* _object_like_macro_h*/ 
